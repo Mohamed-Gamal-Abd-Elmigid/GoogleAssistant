e4541e67c9416d32ee80099c4ba1563a13a660ee
Mohamed-Gamal-Abd-Elmigid <mohamed.gamal.abdelmigid@gmail.com>
2022-03-03 13:28:35 +0200

    add intent to actions xml and mainactivity

---
+++ app/src/main/java/com/example/googleassistant/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.googleassistant
+
+import android.content.Intent
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        intent.handleIntent()
+    }
+
+    private fun Intent.handleIntent(){
+        when(action){
+            Intent.ACTION_VIEW -> handleDeepLink(data)
+            else -> gotoDefaultView()
+        }
+    }
+
+    private fun handleDeepLink(data: Uri?) {
+       when(data?.path)
+       {
+         "/open" -> {
+             val featureType = data.getQueryParameter("featureType").orEmpty()
+             navigatetoActivity(featureType)
+         }
+       }
+    }
+
+    private fun navigatetoActivity(featureType: String) {
+        when(featureType)
+        {
+            "profile" -> {
+                val profileIntent = Intent(this,ProfileActivity::class.java)
+                startActivity(profileIntent)
+            }
+            "settings" -> {
+                val settingsIntent = Intent(this,SettingsActivity::class.java)
+                startActivity(settingsIntent)
+            }
+        }
+
+    }
+
+    private fun gotoDefaultView() {
+        TODO("Not yet implemented")
+    }
+}
