1b94a42f771e3570a3a45694ffacc159c1e7560a
Mohamed-Gamal-Abd-Elmigid <mohamed.gamal.abdelmigid@gmail.com>
2022-03-07 02:44:18 +0200

    fix dynamic link

--- app/src/main/java/com/example/googleassistant/MainActivity.kt
+++ app/src/main/java/com/example/googleassistant/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.etisalat.itworx.activity.R
 import com.etisalat.itworx.activity.ProfileActivity
 import com.etisalat.itworx.activity.SettingsActivity
@@ -15,31 +16,33 @@ class MainActivity : AppCompatActivity() {
         intent.handleIntent()
     }
 
-    private fun Intent.handleIntent(){
-        when(action){
+    private fun Intent.handleIntent() {
+        when (action) {
             Intent.ACTION_VIEW -> handleDeepLink(data)
             else -> gotoDefaultView()
         }
     }
 
     private fun handleDeepLink(data: Uri?) {
-       when(data?.path)
-       {
-         "/open" -> {
-             val featureType = data.getQueryParameter("featureType").orEmpty()
-             navigatetoActivity(featureType)
-         }
-       }
+        Log.d("ttttttt ","Data  = " + data.toString() )
+        when (data?.path) {
+            "/open" -> {
+                val featureType = data.getQueryParameter("featureName").orEmpty()
+                navigatetoActivity(featureType)
+            }
+        }
     }
 
     private fun navigatetoActivity(featureType: String) {
-        when(featureType)
-        {
-            "profile" -> {
+        Log.d("ttttttt ","featureType  = " + featureType )
+
+        when {
+            featureType.contains("profile",true) ->  {
                 val profileIntent = Intent(this, ProfileActivity::class.java)
-                startActivity(profileIntent)
+                 startActivity(profileIntent)
             }
-            "settings" -> {
+
+            featureType.contains("settings",true) -> {
                 val settingsIntent = Intent(this, SettingsActivity::class.java)
                 startActivity(settingsIntent)
             }
